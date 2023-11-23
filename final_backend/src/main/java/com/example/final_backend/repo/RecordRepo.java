d8c4504b322aa4aaeba59b0e8eb1e53394775df7
Yunze Zhao <56664264+Yunzez@users.noreply.github.com>
2023-11-22 22:47:55 -0500

    add get

--- final_backend/src/main/java/com/example/final_backend/repo/RecordRepo.java
+++ final_backend/src/main/java/com/example/final_backend/repo/RecordRepo.java
@@ -1,8 +1,19 @@
 package com.example.final_backend.repo;
 
 import com.example.final_backend.model.Record;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface RecordRepo extends MongoRepository<Record, String> {
     // Define custom query methods here if necessary
-}
+
+    Page<Record> findAll(Pageable pageable);
+
+    @Query("{}")
+    List<Record> findTop100Records();
+}   
