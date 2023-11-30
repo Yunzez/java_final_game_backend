6b042746f8d4113852732e2dc205abb09821e526
Angelo Wu <an9el0wu@gmail.com>
2023-11-30 10:25:24 -0500

    Add userId validation to Record

--- final_backend/src/main/java/com/example/final_backend/ApplicationExceptionHandler.java
+++ final_backend/src/main/java/com/example/final_backend/ApplicationExceptionHandler.java
@@ -1,11 +1,17 @@
 package com.example.final_backend;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import com.example.final_backend.exception.DocumentNotFoundException;
@@ -18,5 +24,12 @@ public class ApplicationExceptionHandler extends ResponseEntityExceptionHandler{
     public ResponseEntity<Object> handleDocumentNotFoundException(DocumentNotFoundException e) {
         ErrorResponse errorResponse = new ErrorResponse(Arrays.asList(e.getMessage()));
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        List<String> errors = new ArrayList<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> errors.add(error.getDefaultMessage()));
+        return new ResponseEntity<>(new ErrorResponse(errors), HttpStatus.BAD_REQUEST);
     }
 }
